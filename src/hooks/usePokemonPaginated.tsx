f0f4f1ae84ebf7894e78e5ed9626be1c2fa1333c
Jose0112Luis <jose0112luis@hotmail.com>
2022-09-12 17:42:28 -0500

    Obtener lista de Pokemons en un Custom Hook usePokemonPaginated.

---
+++ src/hooks/usePokemonPaginated.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from 'react';
+import { pokemonApi } from '../api/pokemonAPI';
+
+export const usePokemonPaginated = () => {
+  
+  const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
+
+  const loadPokemons = async () => {
+    const resp = await pokemonApi.get( nextPageUrl.current );
+    console.log(resp.data);
+    
+  }
+
+  useEffect(() => {
+    loadPokemons();
+  }, [])
+  
+  
+}
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import { Image, Text } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 import { styles } from '../theme/appTheme';
+import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
 
 export const HomeScreen = () => {
 
   const { top } = useSafeAreaInsets();
+  usePokemonPaginated();
 
   return (
     <>
