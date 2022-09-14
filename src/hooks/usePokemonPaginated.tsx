cba3f2ec3d65354e1dfed1cfce4e2f5e4a3f1619
Jose0112Luis <jose0112luis@hotmail.com>
2022-09-14 00:04:14 -0500

    Mostrar pokemons y usar el Infinite Scroll.

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -37,7 +37,8 @@ export const usePokemonPaginated = () => {
   
   return {
     isLoading,
-    simplePokemonList
+    simplePokemonList,
+    loadPokemons,
   }
   
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import { Image, Text } from 'react-native';
+import { ActivityIndicator, FlatList, Image, Text } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-import { styles } from '../theme/appTheme';
 import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
+import { styles } from '../theme/appTheme';
 
 export const HomeScreen = () => {
 
   const { top } = useSafeAreaInsets();
-  const { simplePokemonList } = usePokemonPaginated();
+  const { simplePokemonList, loadPokemons } = usePokemonPaginated();
   
   return (
     <>
@@ -20,7 +20,37 @@ export const HomeScreen = () => {
         }}
       />
       
-      <Text 
+      <FlatList 
+        data={ simplePokemonList }
+        keyExtractor={ ( pokemon ) => pokemon.id }
+        showsVerticalScrollIndicator={ false }
+        renderItem={ ({ item, index }) => (
+          <>
+          <Text>{index} - {item.name}</Text>
+          <Image
+            source={{ uri: item.picture }}
+            style={{ width: 100, height: 100, }}
+          />
+          </>
+        )}
+
+        //infinite scroll
+        onEndReached={ loadPokemons }
+        onEndReachedThreshold={ 0.4 }
+
+        // activity indicator
+        ListFooterComponent={(
+          <ActivityIndicator 
+            style={{
+              height: 100, 
+            }} 
+            size={ 20 }
+            color='grey'
+          /> 
+        )}
+      />
+
+      {/* <Text 
         style={{ 
           ...styles.title,
           ...styles.globalMargin,
@@ -29,7 +59,7 @@ export const HomeScreen = () => {
         }}
       >
         Pokedex
-      </Text>
+      </Text> */}
     </>
   );
 }
