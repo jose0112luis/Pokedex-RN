c322b0794aea879e29ec5b103ffdca85fcf3d2db
Jose0112Luis <jose0112luis@hotmail.com>
2022-09-14 19:35:44 -0500

    Diseño del Header de la pantalla del Pokemon.

--- src/screens/PokemonScreen.tsx
+++ src/screens/PokemonScreen.tsx
@@ -1,19 +1,101 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
+import IconI from 'react-native-vector-icons/Ionicons';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { RootStackParams } from '../navigation/Navigation';
+import { FadeInImage } from '../components/FadeInImage';
 
 interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'> { }
 
-export const PokemonScreen = ( { navigation, route }: Props ) => {
+export const PokemonScreen = ({ navigation, route }: Props) => {
 
   const { simplePokemon, color } = route.params;
+  const { id, name, picture } = simplePokemon;
+  const { top } = useSafeAreaInsets();
 
   return (
     <View>
-      <Text style={{fontSize: 30, color}}>{ simplePokemon.name } - { color }</Text>
+      {/* Header Container */}
+      <View
+        style={{
+          ...styles.headerContainer,
+          backgroundColor: color,
+        }}
+      >
+        {/* Botón Atrás */}
+        <TouchableOpacity
+          onPress={ () => navigation.goBack() }
+          activeOpacity={ 0.7 }
+          style={{ ...styles.btnBack, top: top + 10 }}
+        >
+          <View 
+            style={ styles.bgBtnBack }
+          >
+            <IconI name='arrow-back' size={ 30 } color='#FFF' />
+          </View>
+        </TouchableOpacity>
+
+        {/* Nombre del Pokemón */}
+        <Text style={{ ...styles.pokemonName, top: top +40 }}>
+          { name + '\n' }#{ id }
+        </Text>
+
+        {/* Pokebola Blanca */}
+        <Image 
+          source={ require('../assets/pokebola-blanca.png') }
+          style={ styles.pokebola }
+        />
+
+        {/* Imagen del Pokemón */}
+        <FadeInImage
+          uri={ picture }
+          style={ styles.imgPokemon }
+        />
+
+      </View>
+      { }
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    height: 370,
+    zIndex: 999,
+    alignItems: 'center',
+    borderBottomRightRadius: 1000,
+    borderBottomLeftRadius: 1000,
+  },
+  btnBack: {
+    position: 'absolute',
+    left: 20,
+    // top: 10
+  },
+  bgBtnBack: {
+    backgroundColor: '#23232365', 
+    borderRadius: 50, 
+    width: 40, 
+    height: 40, 
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  pokemonName: {
+    color: '#FFFFFC',
+    fontSize: 40,
+    alignSelf: 'flex-start',
+    left: 20,
+  }, pokebola: {
+    width: 250,
+    height: 250,
+    bottom: -25,
+    opacity: 0.7,
+  },imgPokemon: {
+    width: 250,
+    height: 250,
+    position: 'absolute',
+    bottom: -15,
+  }
+});
